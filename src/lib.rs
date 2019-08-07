3fa75909856bd4d5681e749ee668978d682b8b4b
Tim Hütz <tim@huetz.biz>
2019-08-07 11:34:31 +0200

    docs: add the missing code documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,21 @@
 use std::fmt::{Display, Formatter, Result as FmtResult};
 use std::io::Error;
 
+/// The possible errors which can occur on instantiation of the [HaveIBeenPwnedParser](struct.HaveIBeenPwnedParser.html) class.
 #[derive(Debug)]
 pub enum CreateInstanceError {
+    /// It seems that the format of the file is not as expected.
     Format(FormatErrorKind),
+    /// There was a generic IO error.
     Io(Error),
 }
 
+/// The more specific error if the format could not be read.
 #[derive(Debug)]
 pub enum FormatErrorKind {
+    /// It seems that the file is not a plain text file.
     NotATextFile,
+    /// It seems that the format of at least one of the lines in the file is invalid.
     LineFormatNotCorrect,
 }
 
@@ -35,9 +41,31 @@ impl Display for CreateInstanceError {
     }
 }
 
+/// This class can be used to parse the password files provided by https://haveibeenpwned.com.
 pub struct HaveIBeenPwnedParser;
 
 impl HaveIBeenPwnedParser {
+    /// Get a new instance of the file parsed based on the provided file path.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error in the following situations, but is not
+    /// limited to just these cases:
+    ///
+    ///  * The user does not have the access rights to access the provided file.
+    ///  * The file does not exist.
+    ///  * The file is not a plain text file.
+    ///  * The format of the lines in the text file does not match the required format.
+    ///
+    /// # Example
+    /// ```
+    /// use pwned_rs::HaveIBeenPwnedParser;
+    ///
+    /// match HaveIBeenPwnedParser::from_file("/path/to/the/hash/file.txt") {
+    ///     Ok(instance) => println!("Got an instance of the file parser!"),
+    ///     Err(error) => println!("Could not get an instance, the error was: {}", error)
+    /// }
+    /// ```
     pub fn from_file(path_to_file: &str) -> Result<HaveIBeenPwnedParser, CreateInstanceError> {
         // be sure that the file exists, if not we should return a proper error which the caller can deal with
         let _file_meta_data = match std::fs::metadata(path_to_file) {
