114c46fa3cdd5bd049e7d36c86fee46374c1dc9e
Tim Hütz <tim@huetz.biz>
2019-08-07 07:17:07 +0200

    chore: add basic command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use log::LevelFilter;
+use chrono::Local;
+use clap::{App, crate_version, crate_description, crate_authors, crate_name, load_yaml};
+
+fn initialize_logging() {
+    // configure the logging framework and set the corresponding log level
+    let logging_framework = fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .apply();
+
+    // ensure the logging framework was successfully initialized
+    if logging_framework.is_err() {
+        panic!("Could not initialize the logging framework. Terminating!");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    initialize_logging();
+
+    // configure the command line parser
+    let configuration_parser_config = load_yaml!("cli.yml");
+    let matches = App::from_yaml(configuration_parser_config)
+        .author(crate_authors!())
+        .version(crate_version!())
+        .name(crate_name!())
+        .about(crate_description!())
+        .get_matches();
+
+
 }
