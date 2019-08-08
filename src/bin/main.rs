90a81dca92a9d7b384ea05e12251b0c8f582fb86
Tim Hütz <tim@huetz.biz>
2019-08-08 15:00:29 +0200

    chore: do get an actual instance of the password parser

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,8 @@
 use chrono::Local;
 use clap::{crate_authors, crate_description, crate_name, crate_version, load_yaml, App};
-use log::LevelFilter;
+use log::{error, LevelFilter, debug};
+use pwned_rs::HaveIBeenPwnedParser;
+use std::process::exit;
 
 fn initialize_logging() {
     // configure the logging framework and set the corresponding log level
@@ -29,10 +31,32 @@ fn main() {
 
     // configure the command line parser
     let configuration_parser_config = load_yaml!("cli.yml");
-    let _matches = App::from_yaml(configuration_parser_config)
+    let matches = App::from_yaml(configuration_parser_config)
         .author(crate_authors!())
         .version(crate_version!())
         .name(crate_name!())
         .about(crate_description!())
         .get_matches();
+
+    // get the path to the password file
+    let password_hash_path = match matches.value_of("password-hashes") {
+        Some(path) => path,
+        None => {
+            error!("It seems that the path the the password hashes was not provided, please see the help for usage instructions.");
+            exit(-1);
+        }
+    };
+    debug!("Got {} as a password hash file", password_hash_path);
+
+    // get an instance of the password parser
+    let _parser = match HaveIBeenPwnedParser::from_file(password_hash_path) {
+        Ok(parser) => parser,
+        Err(error) => {
+            error!(
+                "Could not get an instance of the parser. The error was: {}",
+                error
+            );
+            exit(-2);
+        }
+    };
 }
